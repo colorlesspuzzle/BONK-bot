4be1c4b3c4f1bacbd5467d99f85df401b505b80d
Andres Serrano <andresfserrano@gmail.com>
2024-09-30 00:14:32 -0500

    1st commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+// Utiliza el token de Discord almacenado en la variable de entorno DISCORD_TOKEN
+client.login(process.env.DISCORD_TOKEN);
